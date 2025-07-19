fdabe1f17487af9b734994e693c1ef2881fa7ba1
just-being-aryan <aryansachdevaa@gmail.com>
2025-07-19 09:48:49 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,6 +7,8 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  multipleStatements: true,
 });
 
 pool.query('SELECT 1')
